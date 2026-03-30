b834d02a1f0bd1ea7e4b00b91a0180a45d95a424
Gabriel Esquilage <roccaesquil19910@gmail.com>
2026-03-30 16:17:07 -0300

    ajuste para criação de relatorio html, para inclusão no corpo do email

--- k6-portal-aluno/login-portal-test.js
+++ k6-portal-aluno/login-portal-test.js
@@ -1,40 +1,40 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import http from "k6/http";
+import { check, sleep } from "k6";
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import { Trend } from 'k6/metrics';
+import { Trend } from "k6/metrics";
 
-const latencyTrend = new Trend('latency_over_time');
+const latencyTrend = new Trend("latency_over_time");
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 },
-    { duration: '4m', target: 100 },
-    { duration: '2m', target: 200 },
-    { duration: '30s', target: 0 },
+    { duration: "30s", target: 20 },
+    { duration: "4m", target: 100 },
+    { duration: "2m", target: 200 },
+    { duration: "30s", target: 10 },
   ],
 };
 
-const users = JSON.parse(open('./users.json'));
+const users = JSON.parse(open("./users.json"));
 
 export default function () {
   const user = users[__VU % users.length];
 
   const res = http.post(
-    'https://erp-api-dev-922117522963.us-central1.run.app/api-external/v1/portal/auth/login',
+    "https://erp-api-dev-922117522963.us-central1.run.app/api-external/v1/portal/auth/login",
     JSON.stringify({
       login: user.login,
       senha: user.senha,
     }),
     {
-      headers: { 'Content-Type': 'application/json' },
-      timeout: '60s'
-    }
+      headers: { "Content-Type": "application/json" },
+      timeout: "60s",
+    },
   );
 
   latencyTrend.add(res.timings.duration);
 
   check(res, {
-    'status 200': (r) => r.status === 200,
+    "status 200": (r) => r.status === 200,
   });
 
   sleep(1);
